b9abbbf039fe2121ad42f9d0e39df7dc75b7fe28
Joseph Kigozi <kigozijosefed1993@gmail.com>
2024-10-21 19:33:48 +0300

    Successful Sign up

--- app/api/accounts/route.ts
+++ app/api/accounts/route.ts
@@ -18,7 +18,9 @@ const formSchema = z.object({
     password: z.string().min(8, "Password must be at least 8 characters").regex(/[A-Z]/, "Password must contain at least one uppercase letter").regex(/[a-z]/, "Password must contain at least one lowercase letter").regex(/\d/, "Password must contain at least one digit").regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
 });
 
-export async function POST(req:Request) {
+
+
+export default async function POST(req:Request) {
     
     const body = await req.json();
     const {email, name, password, country, parish, subcounty, district} = formSchema.parse(body);
