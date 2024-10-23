d4b4157c50b68fce1ef343f11f2bbec41da101cc
Joseph Kigozi <kigozijosefed1993@gmail.com>
2024-10-23 19:11:47 +0300

    remove clerk

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,26 +1,26 @@
 'use client'
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request, ) {
     
     try {
-        const { userId } = auth();
-        const { title } = await req.json();
+        // const { userId } = auth();
+        // const { title } = await req.json();
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", {status: 401});
-        }
+        // if (!userId) {
+        //     return new NextResponse("Unauthorized", {status: 401});
+        // }
 
-        const course = await db.course.create({
-            data: {
-                userId,
-                title,
-            }
-        });
+        // const course = await db.course.create({
+        //     data: {
+        //         userId,
+        //         title,
+        //     }
+        // });
 
-        return NextResponse.json(course);
+        // return NextResponse.json(course);
+        return NextResponse.json("");
         
     } catch (error) {
         console.log("[COURSES]", error);
